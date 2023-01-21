967eb6c6fff487d3c2b410e60220e877e09cd92a
abhi-shek-bhandari <abhishekbhandari06@outlook.com>
2023-01-21 22:40:40 +0530

    day 5

--- DesiDukaan/src/main/java/com/desidukaan/repository/OrderDao.java
+++ DesiDukaan/src/main/java/com/desidukaan/repository/OrderDao.java
@@ -1,9 +1,21 @@
 package com.desidukaan.repository;
 
+import com.desidukaan.exception.OrderException;
 import com.desidukaan.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.List;
+
 @Repository
-public interface OrderDao extends JpaRepository<Order,String> {
+public interface OrderDao extends JpaRepository<Order, Integer> {
+
+
+    @Query("select o from Orders o where o.orderAddress.city= ?1")
+    public List<Order> getOrderByCity(String city) throws OrderException;
+
+    @Query("select o from Orders o where o.customer.mobileNumber=?1")
+    public List<Order> getOrdersByUserId(int userID);
 }
